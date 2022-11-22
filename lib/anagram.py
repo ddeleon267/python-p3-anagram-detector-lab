42530807588d6edaa37cbc45dc72ecc813425f5a
Dominique DeLeon <dominique.a.deleon@gmail.com>
2022-11-22 14:58:19 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,28 @@
 # your code goes here!
+class Anagram():
+
+    def __init__(self, word):
+        self._word = word
+        return None
+    
+    def match(self, array):
+        # print("SELF IS!", self.word)
+        # return None
+        # turn word into a list and sort
+        word_chars = sorted([letter for letter in self._word])
+        anagrams = []
+        for maybe_match in array:
+            match_chars = sorted([letter for letter in maybe_match])
+            if word_chars == match_chars:
+                anagrams.append(maybe_match)
+        
+        return anagrams
+
+        # go through array
+            # for each item, turn into list and sort
+            # is it equal to the given word?
+#             ## return it as part of a list
+
+## solution passes test, but would give false matches for words with same chars but diff length
+#    def match(self, list):
+#         return [word for word in list if set(word) == set(self._word)]
